aefaeae5efee0bf4bb6734b7bbb46746bec25875
Sylvester Otieno <officialotienosylvester@gmail.com>
2024-08-05 16:23:36 +0300

    Declaring Variables in GO using keyword Var

--- Language/GO/main.go
+++ Language/GO/main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World, it's time to learn go")
+
+	//Variables
+	// Strings
+
+	var member_name string = "Mr Golf Member"
+	var executiveMemberName = "Mr CEO"
+	var SeniorBoardMember string
+	fmt.Println(member_name, executiveMemberName, SeniorBoardMember)
+
 }
