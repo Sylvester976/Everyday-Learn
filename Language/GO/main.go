e31c7831bfd86755a9a5e145b2e8fe60ec82eb30
Sylvester Otieno <officialotienosylvester@gmail.com>
2024-08-13 20:10:44 +0300

    Slices are more like arrays in other languages

--- Language/GO/main.go
+++ Language/GO/main.go
@@ -12,5 +12,11 @@ func main() {
 	fmt.Println(names)
 
 	//slices in Go
+	scores := []int{20, 21, 22}
+	fmt.Println(scores, len(scores))
+	scores[2] = 87
+	fmt.Println(scores)
+	scores = append(scores, 85)
+	fmt.Println(scores, len(scores))
 
 }
