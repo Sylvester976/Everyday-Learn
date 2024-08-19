1f0626de90e7233acfd66f04b36bc8450eb3b6e8
Sylvester Otieno <officialotienosylvester@gmail.com>
2024-08-19 17:05:20 +0300

    Standard Libraries String library

--- Language/GO/main.go
+++ Language/GO/main.go
@@ -11,4 +11,6 @@ func main() {
 	fmt.Println(salutations, strings.Contains(salutations, "Hello"))
 	fmt.Println(salutations, strings.ReplaceAll(salutations, "Hello", "Salut"))
 	fmt.Println(salutations, strings.ToUpper(salutations))
+	fmt.Println(strings.Index(salutations, "0"))
+	fmt.Println(strings.Split(salutations, " pi"))
 }
