64c4716a967c0455f6ab2782991dea96293fc71c
Sylvester Otieno <officialotienosylvester@gmail.com>
2024-08-19 16:55:00 +0300

    Standard Libraries

--- Language/GO/main.go
+++ Language/GO/main.go
@@ -1,28 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	//Arrays and Slices
-	var ages [3]int = [3]int{40, 20, 30}
-	var new_age = [3]int{40, 20, 30}
-	fmt.Println(ages, new_age, len(ages), len(new_age))
-
-	names := [3]string{"My", "Name", "Is"}
-	fmt.Println(names)
-
-	//slices in Go
-	scores := []int{20, 21, 22}
-	fmt.Println(scores, len(scores))
-	scores[2] = 87
-	fmt.Println(scores)
-	scores = append(scores, 85)
-	fmt.Println(scores, len(scores))
-
-	//slices ranges
-	rangeOne := names[1:]
-	fmt.Println(rangeOne)
-
-	rangeOne = append(rangeOne, "Peris")
-	fmt.Println(rangeOne)
+	//Standard Libraries
+	salutations := "Hello World"
+	fmt.Println(salutations, strings.Contains(salutations, "Hello"))
+	fmt.Println(salutations, strings.ReplaceAll(salutations, "Hello", "Salut"))
+	fmt.Println(salutations, strings.ToUpper(salutations))
 }
