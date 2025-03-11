c91ddbba50f831576fec987bf45413f7623561b7
Sylvester Nyabenge <officialotienosylvester@gmail.com>
2025-03-11 14:58:28 +0300

    Create learn.py

---
+++ Language/python/learn.py
@@ -0,0 +1,71 @@
+print('Mustaaaaaaaarddddddd!!!!! \n turn the tv off')
+print( '*'*10    )
+
+# The `zone` variable is storing a multi-line string that contains the lyrics from the song "All of
+# the Lights" by Kanye West.
+zone = """  
+All of the lights 
+something wrong mj gone, that nigga dead
+"""
+
+
+print(zone.upper())
+print(zone.lower())
+print(zone.title())
+print(zone.strip())
+
+Loops in python
+For loop
+
+looper = [1,2,3,4]
+
+for loopers in looper:
+    print(loopers)
+    
+# For loop using range 
+for num in range(20):
+    print (str(num) +'\n')
+    
+for someNum in range(7,21):
+    print(str(someNum) +'\n')
+    
+# WHILE LOOP
+    incre = 1
+    while incre < 20:
+        print(incre)
+        incre += 2
+        
+
+Prints out 0,1,2,3,4
+
+count = 0
+while True:
+    print(count)
+    count += 1
+    if count >= 5:
+        break
+
+# Prints out only odd numbers - 1,3,5,7,9
+for x in range(10):
+    # Check if x is even
+    if x % 2 == 0:
+        continue
+    print(x)
+
+for else
+
+successful = False
+for number in range(3):
+    print("Attempt") 
+    if successful:
+        print("successful")
+        break
+else:
+    print("Attempted 3 times and failed")    
+
+Nested loop
+for x in range(5):
+    for y in range(3):
+        print(f"{x},{y}")
+
+# iterables 
