9b4a5b3cd9e86169d5b34c2e243e60dcf9c84f18
sylvester976 <officialotienosylvester@gmail.com>
2025-10-07 16:48:33 +0300

    JS welcome to the list

---
+++ Language/javascript/learn.js
@@ -0,0 +1,28 @@
+console.log('Hello World!');
+
+// comments -single line
+
+/*
+comments-multi-line
+*/
+
+
+//data types
+console.log('JavaScript')
+console.log(2011)
+console.log('Woohoo! I love to code! #codecademy')
+console.log(20.49)
+
+//Arithmetic Operators
+console.log(27+3.5)
+console.log(2025-1969)
+console.log(65/240)
+console.log(0.2708*100)
+
+
+// String Concatenation
+console.log('Hello' + 'World')
+console.log('Hello' + ' ' + 'World')
+
+// Properties
+console.log('Teaching the world how to code'.length)
