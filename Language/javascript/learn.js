8a7d6f6f7ad84c7578d96a34f52dc39c0954417c
sylvester976 <officialotienosylvester@gmail.com>
2025-10-07 17:08:11 +0300

    Learning variables

--- Language/javascript/learn.js
+++ Language/javascript/learn.js
@@ -26,3 +26,22 @@ console.log('Hello' + ' ' + 'World')
 
 // Properties
 console.log('Teaching the world how to code'.length)
+
+//methods
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+// Built-in Objects
+console.log(Math.random())
+console.log(Math.floor(Math.random() * 100))
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
+
+//variables
+var favoriteFood = 'pizza'
+var numOfSlices = 8
+console.log(favoriteFood);
+console.log(numOfSlices)
