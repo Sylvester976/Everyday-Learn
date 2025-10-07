dff1c9b3c1176e0cd846c762b159056697a8d672
sylvester976 <officialotienosylvester@gmail.com>
2025-10-07 17:23:16 +0300

    Variables let and const

--- Language/javascript/learn.js
+++ Language/javascript/learn.js
@@ -41,7 +41,16 @@ console.log(Math.ceil(43.8));
 console.log(Number.isInteger(2017));
 
 //variables
-var favoriteFood = 'pizza'
-var numOfSlices = 8
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
 console.log(favoriteFood);
 console.log(numOfSlices)
+
+//Create a Variable: let
+let changeMe = true;
+changeMe = false;
+console.log(changeMe)
+
+// Create a Variable: const
+const entree = 'Enchiladas';
+console.log(entree)
